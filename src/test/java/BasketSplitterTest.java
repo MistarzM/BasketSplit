164570d15faf8293e4721825623b4de22584338e
Michał Mistarz <michal.mistarz02@gmail.com>
2024-04-07 09:08:30 +0200

    Adds relative paths to testing files

--- src/test/java/BasketSplitterTest.java
+++ src/test/java/BasketSplitterTest.java
@@ -7,16 +7,16 @@ class BasketSplitterTest {
 
     @Test
     void testSplitBasket1() throws Exception {
-        BasketSplitter basketSplitter = new BasketSplitter("C:/Users/micha/OneDrive/Pulpit/BasketSpliter/config.json");
-        Map<String, List<String>> result = basketSplitter.split("C:/Users/micha/OneDrive/Pulpit/BasketSpliter/basket-1.json");
+        BasketSplitter basketSplitter = new BasketSplitter("Zadanie/config.json");
+        Map<String, List<String>> result = basketSplitter.split("Zadanie/basket-1.json");
         System.out.println(result);
         assertNotNull(result);
     }
 
     @Test
     void testSplitBasket2() throws Exception {
-        BasketSplitter basketSplitter = new BasketSplitter("C:/Users/micha/OneDrive/Pulpit/BasketSpliter/config.json");
-        Map<String, List<String>> result = basketSplitter.split("C:/Users/micha/OneDrive/Pulpit/BasketSpliter/basket-2.json");
+        BasketSplitter basketSplitter = new BasketSplitter("Zadanie/config.json");
+        Map<String, List<String>> result = basketSplitter.split("Zadanie/basket-2.json");
         System.out.println(result);
         assertNotNull(result);
     }
